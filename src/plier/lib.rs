deb42437a067743a4ee29509a0a57b4b768d9126
shaolang <shaolang@ymail.com>
2020-05-05 14:07:57 +0800

    Make test runnable on all OSes

--- src/plier/lib.rs
+++ src/plier/lib.rs
@@ -1,9 +1,8 @@
 use std::path::PathBuf;
-use std::str::FromStr;
 
 pub fn batch_filename(exe_path: PathBuf, filename: &str) -> PathBuf {
     let exe_str = exe_path.to_str().expect("Unable to get binary's path");
-    let mut file = PathBuf::from_str(exe_str).unwrap().parent()
+    let mut file = PathBuf::from(exe_str).parent()
         .expect("Unable to get binary's directory").to_owned();
     file.push(filename);
     file
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,11 +1,13 @@
 use std::path::PathBuf;
-use std::str::FromStr;
 use plier;
 
 #[test]
 fn batch_filename_combines_executables_directory_with_given_filename() {
-    let exe_path = PathBuf::from_str("path/to/binary.exe").unwrap();
+    let exe_path = PathBuf::from("path/to/binary.exe");
     let actual = plier::batch_filename(exe_path, "batch.sh");
+    let mut expected = PathBuf::from("path");
+    expected.push("to");
+    expected.push("batch.sh");
 
-    assert_eq!(actual, PathBuf::from_str("path\\to\\batch.sh").unwrap());
+    assert_eq!(actual, expected);
 }
