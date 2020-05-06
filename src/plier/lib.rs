7e4cb8c12124dbd1a468e04a19856857f2d03ddb
shaolang <shaolang@ymail.com>
2020-05-06 12:23:14 +0800

    Implement plier::create_spec

--- src/plier/lib.rs
+++ src/plier/lib.rs
@@ -1,4 +1,6 @@
+use std::collections::BTreeMap;
 use std::path::PathBuf;
+use toml::{self, Value};
 
 pub fn batch_filename(exe_path: PathBuf, filename: &str) -> PathBuf {
     let exe_str = exe_path.to_str().expect("Unable to get binary's path");
@@ -6,4 +8,21 @@ pub fn batch_filename(exe_path: PathBuf, filename: &str) -> PathBuf {
         .expect("Unable to get binary's directory").to_owned();
     file.push(filename);
     file
+}
+
+
+pub fn create_spec(existing: &str, name: &str, bins: Vec<String>)
+        -> Result<String, toml::ser::Error> {
+    let v: Value = toml::from_str(existing).unwrap();
+    let mut m: BTreeMap<String, BTreeMap<String, Value>> = v.try_into().unwrap();
+
+    let bins: Vec<Value> = bins.iter()
+        .map(|s| Value::String(s.to_string()))
+        .collect();
+    let mut sm: BTreeMap<String, toml::Value> = BTreeMap::new();
+
+    sm.insert("bins".to_string(), toml::Value::Array(bins));
+    m.insert(name.to_string(), sm);
+
+    toml::to_string(&m)
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use indoc::indoc;
 use plier;
 
 #[test]
@@ -8,6 +9,34 @@ fn batch_filename_combines_executables_directory_with_given_filename() {
     let mut expected = PathBuf::from("path");
     expected.push("to");
     expected.push("batch.sh");
+
+    assert_eq!(actual, expected);
+}
+
+
+#[test]
+fn create_spec_for_new_devkit() {
+    let actual = plier::create_spec("", "java", vec!["bin".to_string()]);
+    let expected = Ok(indoc!(r#"[java]
+                                bins = ["bin"]
+                                "#).to_string());
+
+    assert_eq!(actual, expected);
+}
+
+
+#[test]
+fn create_spec_when_others_exists() {
+    let actual = plier::create_spec(
+        "[java]\nbins = [\"bin\"]\n",
+        "python",
+        vec![".".to_string(), "Scripts".to_string()]);
+    let expected = Ok(indoc!(r#"[java]
+                                bins = ["bin"]
+
+                                [python]
+                                bins = [".", "Scripts"]
+                                "#).to_string());
 
     assert_eq!(actual, expected);
 }
