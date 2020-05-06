f072b26a0d4375354c88089389940949d236c534
shaolang <shaolang@ymail.com>
2020-05-06 12:37:06 +0800

    Add struct Spec to simplify create_spec and make spec field explicit

--- src/plier/lib.rs
+++ src/plier/lib.rs
@@ -1,6 +1,13 @@
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 use toml::{self, Value};
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Spec {
+    bins: Vec<String>,
+}
+
 
 pub fn batch_filename(exe_path: PathBuf, filename: &str) -> PathBuf {
     let exe_str = exe_path.to_str().expect("Unable to get binary's path");
@@ -14,15 +21,8 @@ pub fn batch_filename(exe_path: PathBuf, filename: &str) -> PathBuf {
 pub fn create_spec(existing: &str, name: &str, bins: Vec<String>)
         -> Result<String, toml::ser::Error> {
     let v: Value = toml::from_str(existing).unwrap();
-    let mut m: BTreeMap<String, BTreeMap<String, Value>> = v.try_into().unwrap();
-
-    let bins: Vec<Value> = bins.iter()
-        .map(|s| Value::String(s.to_string()))
-        .collect();
-    let mut sm: BTreeMap<String, toml::Value> = BTreeMap::new();
-
-    sm.insert("bins".to_string(), toml::Value::Array(bins));
-    m.insert(name.to_string(), sm);
+    let mut m: BTreeMap<String, Spec> = v.try_into().unwrap();
+    m.insert(name.to_string(), Spec { bins });
 
     toml::to_string(&m)
 }
