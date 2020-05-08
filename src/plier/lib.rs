826d113efc86d3f3ba092f2f2eb0e13669a6c430
shaolang <shaolang@ymail.com>
2020-05-08 14:06:56 +0800

    Adopt Rust's idiom in keeping unit tests together with src
    /tests is for integration tests.

--- src/plier/lib.rs
+++ src/plier/lib.rs
@@ -44,3 +44,105 @@ pub fn load_spec(s: &str) -> Result<Spec, toml::de::Error> {
     let v: Value = toml::from_str(s).unwrap();
     v.try_into::<Spec>()
 }
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use std::path::PathBuf;
+
+    #[test]
+    fn batch_filename_combines_executables_directory_with_given_filename() {
+        let exe_path = PathBuf::from("path/to/binary.exe");
+        let actual = super::batch_filename(exe_path, "batch.sh");
+        let mut expected = PathBuf::from("path");
+        expected.push("to");
+        expected.push("batch.sh");
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn add_spec_for_new_devkit() {
+        let mut actual = super::load_spec("").unwrap();
+        super::add_spec_entry(&mut actual, "java", vec!["bin".to_string()]);
+        let expected = super::load_spec(indoc!(
+            r#"[java]
+               bins = ["bin"]
+               "#
+        ))
+        .unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn add_spec_when_others_exists() {
+        let mut actual = super::load_spec("[java]\nbins = [\"bin\"]\n").unwrap();
+        super::add_spec_entry(
+            &mut actual,
+            "python",
+            vec![".".to_string(), "Scripts".to_string()],
+        );
+        let expected = super::load_spec(indoc!(
+            r#"[java]
+               bins = ["bin"]
+
+               [python]
+               bins = [".", "Scripts"]
+               "#
+        ))
+        .unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn load_spec_from_empty_string() {
+        let actual = super::load_spec("").unwrap();
+
+        assert_eq!(actual, super::Spec::new());
+    }
+
+    #[test]
+    fn load_spec_from_valid_spec_string() {
+        let actual = super::load_spec(indoc!(
+            r#"[java]
+               bins = ["bin"]
+
+               [python]
+               bins = [".", "Scripts"]
+               "#
+        ))
+        .unwrap();
+        let mut expected = super::Spec::new();
+
+        expected.insert(
+            "java".to_string(),
+            super::SpecEntry {
+                bins: vec!["bin".to_string()],
+            },
+        );
+        expected.insert(
+            "python".to_string(),
+            super::SpecEntry {
+                bins: vec![".".to_string(), "Scripts".to_string()],
+            },
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn load_spec_from_invalid_spec_string() {
+        let actual = super::load_spec(indoc!(
+            r#"[hello]
+               bins = ['sbin']
+
+               [world]
+               greeting = ['what?']
+               "#
+        ));
+
+        assert!(actual.is_err());
+    }
+}
