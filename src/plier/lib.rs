089284b8011eb740eeaad502796025e83df7a3f4
shaolang <shaolang@ymail.com>
2020-06-20 23:11:48 +0800

    Implement fmt::Display trait for PlierSpec

--- src/plier/lib.rs
+++ src/plier/lib.rs
@@ -13,26 +13,28 @@
  * limitations under the License.
  */
 
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt;
 use std::path::PathBuf;
+use toml;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct PlierSpec {
     apps: HashMap<String, AppSpec>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct AppSpec {
     home_name: String,
     bins: Vec<String>,
     versions: Option<Vec<Version>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Version {
     version: String,
-    path: PathBuf,
+    home_path: PathBuf,
 }
 
 impl PlierSpec {
@@ -47,9 +49,17 @@ impl PlierSpec {
     }
 }
 
+impl fmt::Display for PlierSpec {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", toml::to_string(&self).unwrap())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
+    use std::collections::HashMap;
+    use std::path::PathBuf;
 
     #[test]
     fn load_returns_empty_apps_map_when_input_is_empty() {
@@ -79,11 +89,42 @@ mod tests {
 
                [[apps.python.versions]]
                version = "3.7.0"
-               path = "/path/to/python/3.7.0"
+               home_path = "/path/to/python/3.7.0"
                "#
         ));
         let versions = spec.apps.get("python").unwrap().versions.as_ref();
 
         assert_eq!(versions.unwrap()[0].version, "3.7.0");
+    }
+
+    #[test]
+    fn fmt_converts_spec_to_string() {
+        let version = super::Version {
+            version: "1.10.3".to_string(),
+            home_path: PathBuf::from("/path/to/elixir/1.10.3"),
+        };
+        let app_spec = super::AppSpec {
+            home_name: "elixir_home".to_string(),
+            bins: vec!["bin".to_string()],
+            versions: Some(vec![version]),
+        };
+        let mut map: HashMap<String, super::AppSpec> = HashMap::new();
+        map.insert("elixir".to_string(), app_spec);
+
+        let spec = super::PlierSpec { apps: map };
+
+        assert_eq!(
+            format!("{}", &spec),
+            indoc!(
+                r#"[apps.elixir]
+                   home_name = "elixir_home"
+                   bins = ["bin"]
+
+                   [[apps.elixir.versions]]
+                   version = "1.10.3"
+                   home_path = "/path/to/elixir/1.10.3"
+                   "#
+            )
+        )
     }
 }
